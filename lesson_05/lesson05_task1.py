2ef850083ea97072b2d92072da39ab1ef52f5051
MarinaGulevich <MarinaGulevich1@yandex.ru>
2025-12-06 21:24:03 +0700

    DZ-5.1

--- lesson_05/lesson05_task1.py
+++ lesson_05/lesson05_task1.py
@@ -1,27 +1,12 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 driver = webdriver.Chrome()
 
+driver.maximize_window()
 driver.get("http://uitestingplayground.com/classattr")
 
-blue_button = driver.find_element(
-    By.CSS_SELECTOR, ' button[class="btn class3 btn-primary btn-test"]')
-blue_button.click()
-
-# РЅР°Р¶Р°С‚СЊ РЅР° РєРЅРѕРїРєСѓ "РћРє" РІСЃРїР»С‹РІР°СЋС‰РµРіРѕ РѕРєРЅР° РІ СЂСѓС‡РЅСѓСЋ
-sleep(2)
-
-blue_button = driver.find_element(
-    By.CSS_SELECTOR, ' button[class="btn class3 btn-primary btn-test"]')
-blue_button.click()
-# РЅР°Р¶Р°С‚СЊ РЅР° РєРЅРѕРїРєСѓ "РћРє" РІСЃРїР»С‹РІР°СЋС‰РµРіРѕ РѕРєРЅР° РІ СЂСѓС‡РЅСѓСЋ
-sleep(2)
-
-blue_button = driver.find_element(
-    By.CSS_SELECTOR, ' button[class="btn class3 btn-primary btn-test"]')
-blue_button.click()
+button = driver.find_element(By.CSS_SELECTOR, '.btn-primary')
+button.click()
 
 driver.quit()
